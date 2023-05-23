3a9be3ac3d7a69382534e024f83329e90b335c56
seungmin-dev <seungmin.dev@gmail.com>
2023-05-23 14:37:25 +0900

    초기 로딩 화면 추가

--- src/components/board.tsx
+++ src/components/board.tsx
@@ -11,6 +11,7 @@ import {
   query,
   where,
   orderBy,
+  and,
 } from "firebase/firestore/lite";
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
@@ -44,23 +45,26 @@ const Board = () => {
   //데이터 한번에 읽어오기
   const getData = async () => {
     // 3일 이내 조회
-    // const q1 = query(
-    //   collection(db, "wauwt"),
-    //   where("createdAt", ">=", Number(new Date()) / 1000 - 259200),
-    //   orderBy("createdAt", "desc")
-    // );
-    const q2 = query(
+    const q1 = query(
       collection(db, "wauwt"),
-      where("location.lat", ">=", Math.abs(location.lat - 0.011)),
-      where("location.lat", "<=", Math.abs(location.lat + 0.011))
+      //   where("createdAt", ">=", Number(new Date()) / 1000 - 259200),
+      orderBy("createdAt", "desc")
     );
+    // const q2 = query(
+    //   collection(db, "wauwt"),
+    //   and(
+    //     where("location.lat", ">=", Math.abs(location.lat - 0.011)),
+    //     where("location.lat", "<=", Math.abs(location.lat + 0.011))
+    //   )
+    // );
     // const q3 = query(
     //   collection(db, "wauwt"),
     //   where("location.lon", ">=", Math.abs(location.lon - 0.011)),
     //   where("location.lon", "<=", Math.abs(location.lon + 0.011))
     // );
 
-    let querySnapShot = await getDocs(q2);
+    let querySnapShot = await getDocs(q1);
+    querySnapShot.docs.forEach((doc) => console.log(doc.data()));
     const data = querySnapShot.docs.map((doc) => ({
       id: doc.id,
       ...(doc.data() as Data),
@@ -95,45 +99,43 @@ const Board = () => {
             data.uid === userInfo.uid ? "bg-blue-100" : "bg-white"
           )}
         >
-          {
-            // Math.abs(data.location.lat - location.lat) <= 0.001 &&
-            Math.abs(data.location.lon - location.lon) <= 0.011 ? (
-              <>
-                {data.reportedCount >= 3 ? (
-                  <h2>신고가 3회 이상 접수되어 가려진 글입니다.</h2>
-                ) : (
-                  <>
-                    <h4 className="font-semibold text-lg pb-1">
-                      랜선친구 {data.randomNum}
-                    </h4>
-                    <p className="mb-1">{data.content}</p>
-                    <div className="flex justify-between">
-                      <p className="text-zinc-600 text-sm">
-                        {timeStampConvertor(data.createdAt)}
-                      </p>
-                      {data.uid === userInfo.uid ? (
-                        <button
-                          onClick={() => onDelete(data.id)}
-                          className="p-1 px-2 text-white bg-red-600 rounded-md text-sm"
-                        >
-                          삭제
-                        </button>
-                      ) : (
-                        <button
-                          onClick={() => onReport(data.id)}
-                          className="p-1 px-2 text-white bg-red-600 rounded-md text-sm"
-                        >
-                          신고
-                        </button>
-                      )}
-                    </div>
-                  </>
-                )}
-              </>
-            ) : (
-              "현재 위치에서는 볼 수 없는 글입니다."
-            )
-          }
+          {Math.abs(data.location.lat - location.lat) <= 0.001 &&
+          Math.abs(data.location.lon - location.lon) <= 0.011 ? (
+            <>
+              {data.reportedCount >= 3 ? (
+                <h2>신고가 3회 이상 접수되어 가려진 글입니다.</h2>
+              ) : (
+                <>
+                  <h4 className="font-semibold text-lg pb-1">
+                    랜선친구 {data.randomNum}
+                  </h4>
+                  <p className="mb-1">{data.content}</p>
+                  <div className="flex justify-between">
+                    <p className="text-zinc-600 text-sm">
+                      {timeStampConvertor(data.createdAt)}
+                    </p>
+                    {data.uid === userInfo.uid ? (
+                      <button
+                        onClick={() => onDelete(data.id)}
+                        className="p-1 px-2 text-white bg-red-600 rounded-md text-sm"
+                      >
+                        삭제
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => onReport(data.id)}
+                        className="p-1 px-2 text-white bg-red-600 rounded-md text-sm"
+                      >
+                        신고
+                      </button>
+                    )}
+                  </div>
+                </>
+              )}
+            </>
+          ) : (
+            "현재 위치에서는 볼 수 없는 글입니다."
+          )}
         </div>
       ))}
     </div>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,38 @@
 import Image from "next/image";
 import { useRecoilState } from "recoil";
 import { weatherBgState } from "@/util/atom";
+import { cls } from "@/util/utils";
+import loadingGif from "../../public/images/loading-gif.gif";
+import { useEffect, useState } from "react";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 export default function Layout({ children }: LayoutProps) {
   const [weatherBg, setWeatherBg] = useRecoilState(weatherBgState);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    if (weatherBg !== "default") setLoading(false);
+    else setLoading(true);
+  }, [loading, weatherBg]);
   return (
-    <div className="w-full h-screen py-32">
-      <div className="w-[28rem] h-[48rem] bg-white/60 m-auto rounded-3xl relative">
+    <div
+      className={cls(
+        weatherBg === "default" ? "" : "py-32",
+        "w-full h-screen overflow-hidden"
+      )}
+    >
+      {loading ? (
+        <div className="w-full h-screen bg-blue-200 flex items-center flex-col justify-center">
+          <h2 className="text-white text-2xl pb-10">
+            날씨 정보를 로딩 중입니다
+          </h2>
+          <Image src={loadingGif} alt="" width={40} />
+        </div>
+      ) : (
+        ""
+      )}
+      <div className="w-[28rem] h-[48rem] bg-white/60 m-auto rounded-3xl relative shadow-2xl">
         {children}
       </div>
       <div>
